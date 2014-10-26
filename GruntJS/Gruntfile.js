1e62ba481054ca39a958adbfa55384af019beb69
SanaNasar <sanasehereen.cse@gmail.com>
2014-10-26 15:18:55 -0700

    Creating a simple task and testing with grunt

--- GruntJS/Gruntfile.js
+++ GruntJS/Gruntfile.js
@@ -1,9 +1,21 @@
 module.exports = function(grunt) {
 
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+
   // Project configuration.
    grunt.registerTask("hello", "Prints hello world", function(){
     grunt.log.writeln("hello world!"); // login utility
 
+   });
+
+   grunt.config("cssmin", {
+    combine: {
+      'app/stylesheets/app.min.css' : [
+        'app/components/bootstrap/dist/css/bootstrap.min.css',
+          'app/stylesheets/*.css'
+            ]
+
+    }
    });
 
 };
