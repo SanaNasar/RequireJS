d61e9d00886931e17d61907bd0598944f9424c29
SanaNasar <sanasehereen.cse@gmail.com>
2014-10-26 16:22:00 -0700

    Adding views folder with index html file

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,15 +1,26 @@
 var express = require('express');
+var ejs = require('ejs');
 var app = express();
 
+// app.engine("html", ejs.renderFile);
+app.set("view engine", "ejs");
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render("index.html", ());
+
+  // res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+// starting a server and listening on port 3000
+app.listen("3000", function(){
+  console.log("Server running on 3000!");
 });
+
+// var server = app.listen(3000, function () {
+
+//   var host = server.address().address;
+//   var port = server.address().port;
+
+//   console.log('Example app listening at http://%s:%s', host, port);
+
+// });
